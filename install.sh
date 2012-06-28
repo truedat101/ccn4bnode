52336fd1292250c02af4c9ae07bdba10b42b3fbc
David J. Kordsmeier <dkords@gmail.com>
2012-06-28 13:24:06 -0700

    Forgot to push this thing up, bumped version to 0.2.0, added hooks for 0.6.0 CCNx.  Fixed the test hook.

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ echo "==========================================================================
 echo "=                                                                              ="
 echo "=                                                                              ="
 echo "================================================================================"
-echo "grab the latest CCNx package v0.4.0"
+echo "grab the latest CCNx package v0.6.0"
 cd external
-wget https://github.com/ProjectCCNx/ccnx/zipball/ccnx-0.4.0 -O ccnx-0.4.0.zip
-unzip ccnx-0.4.0.zip
-mv ProjectCCNx* ccnx-0.4.0
-cd ccnx-0.4.0
+wget https://github.com/ProjectCCNx/ccnx/zipball/ccnx-0.6.0 -O ccnx-0.6.0.zip
+unzip ccnx-0.6.0.zip
+mv ProjectCCNx* ccnx-0.6.0
+cd ccnx-0.6.0
 if [ x`which gmake` != "x" ]; then
   echo "Using GNU make";
   gmake
--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 var assert = require('assert')
-  , ccn4bnode = require(__dirname + '../lib/index.js');
+  , ccn4bnode = require(__dirname + '/../index.js');
 
 
 //
 // Hey jerky, write some tests.
+//
 assert.ok(true);
 assert.ok(ccn4bnode);
